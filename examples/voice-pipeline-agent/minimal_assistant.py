c08c8b114c0b0c11142d9c173562a9e401a01a02
liuxianqi <liuxianqi@360.cn>
2024-10-08 16:38:57 +0800

    init study

--- examples/voice-pipeline-agent/minimal_assistant.py
+++ examples/voice-pipeline-agent/minimal_assistant.py
@@ -1,5 +1,7 @@
 import asyncio
+import datetime
 import logging
+from typing import AsyncIterable
 
 from dotenv import load_dotenv
 from livekit import rtc
@@ -12,7 +14,7 @@ from livekit.agents import (
     llm,
 )
 from livekit.agents.pipeline import VoicePipelineAgent
-from livekit.plugins import deepgram, openai, silero
+from livekit.plugins import deepgram, openai, silero, qwen
 
 load_dotenv()
 logger = logging.getLogger("voice-assistant")
@@ -26,8 +28,10 @@ async def entrypoint(ctx: JobContext):
     initial_ctx = llm.ChatContext().append(
         role="system",
         text=(
-            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
-            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+            # "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            # "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+            "你是一个语音助手，你的主要语言是中文，你与用户的界面将是语音。"
+            "你应该使用简短的回答，并避免使用难发音的汉字。"
         ),
     )
 
@@ -38,17 +42,42 @@ async def entrypoint(ctx: JobContext):
     participant = await ctx.wait_for_participant()
     logger.info(f"starting voice assistant for participant {participant.identity}")
 
-    dg_model = "nova-2-general"
-    if participant.kind == rtc.ParticipantKind.PARTICIPANT_KIND_SIP:
-        # use a model optimized for telephony
-        dg_model = "nova-2-phonecall"
+    ## TODO - deepgram
+    # dg_model = "nova-2-general"
+    # if participant.kind == rtc.ParticipantKind.PARTICIPANT_KIND_SIP:
+    #     # use a model optimized for telephony
+    #     dg_model = "nova-2-phonecall"
+
+    ## TODO
+    def _before_tts_cb(agent: VoicePipelineAgent, text: str | AsyncIterable[str]):
+        if isinstance(text, str):
+            return str.replace(text,"。", ".").replace("，", ".").replace("！", "!").replace("；",".").replace("？","?")
+        else:
+            async def _iterate_str():
+                async for chunk in text:
+                    yield chunk.replace("。", ".").replace("，", ".").replace("！", "!").replace("；",".").replace("？","?")
+
+            return _iterate_str()
 
     agent = VoicePipelineAgent(
         vad=ctx.proc.userdata["vad"],
-        stt=deepgram.STT(model=dg_model),
-        llm=openai.LLM(),
-        tts=openai.TTS(),
+        # stt=deepgram.STT(model=dg_model),
+        # llm=openai.LLM(),
+        # tts=openai.TTS(),
+
+        ## TODO - qwen2
+        stt=openai.STT(base_url="http://10.218.127.99:3000/", language="zn"),
+        llm=openai.LLM(base_url="http://10.218.127.29:11434/v1/", model="qwen2:72b"),
+        tts=qwen.TTS(
+            seed=42,
+            style_type="中文女",
+            base_url="http://10.218.127.100:3000/instruct/synthesize",
+            prompt="A girl speaker with a brisk pitch, an enthusiastic speaking pace, and a upbeat emotional demeanor.",
+        ),
+
         chat_ctx=initial_ctx,
+
+        before_tts_cb=_before_tts_cb,
     )
 
     agent.start(ctx.room, participant)
@@ -58,6 +87,7 @@ async def entrypoint(ctx: JobContext):
     chat = rtc.ChatManager(ctx.room)
 
     async def answer_from_text(txt: str):
+        print("LLM input timestamp", txt, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
         chat_ctx = agent.chat_ctx.copy()
         chat_ctx.append(role="user", text=txt)
         stream = agent.llm.chat(chat_ctx=chat_ctx)
@@ -68,7 +98,13 @@ async def entrypoint(ctx: JobContext):
         if msg.message:
             asyncio.create_task(answer_from_text(msg.message))
 
-    await agent.say("Hey, how can I help you today?", allow_interruptions=True)
+    # await agent.say("Hey, how can I help you today?", allow_interruptions=True)
+
+    ## TODO
+    test = "你好，有什么我可以帮你的吗？"
+    # test = "欢迎使用360智汇云RTC驱动的大模型AI语音助手"
+    # test = "Hey, how can I help you today?"
+    await agent.say(test, allow_interruptions=True)
 
 
 if __name__ == "__main__":
